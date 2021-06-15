39d98f1b945d0e2887284649851bb7f912c9f99f
pavantamidala <pavantamidala023@gmail.com>
2021-06-15 07:07:05 +0530

    completed two solutions

--- array/findInicesBasedOnTarget.js
+++ array/findInicesBasedOnTarget.js
@@ -1,7 +1,7 @@
 // Given an array of integers return the indices of the two numbers that add up to a given target
 
 // my solution
-let arr = [1, 3, 2, 9, 10, 8]
+let arr = [1, 3,9, 10, 8,2]
 let target = 5
 function findInteger(array, target) {
     for (let i = 0; i < arr.length; i++) {
@@ -12,4 +12,21 @@ function findInteger(array, target) {
         }
     }
 }
-console.log(findInteger(arr, target))
+// console.log(findInteger(arr, target))
+
+//other solutions not mine
+
+function findInteger2(array,target){
+    for(let i=0;i<array.length;i++){
+        let numberToFind = target - array[i]
+        
+        for(let j=i+1;j<array.length;j++){
+            if (array[j] === numberToFind) {
+                return [i, j]
+            }
+        }
+    }
+    return null
+}
+console.log(findInteger2(arr,target))
+
